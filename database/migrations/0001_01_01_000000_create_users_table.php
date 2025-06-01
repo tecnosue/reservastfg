5397c17e0633b15bae6321fbbbf11e0d657bb477
tecnosue <susanaparacuellos@gmail.com>
2025-06-01 21:28:09 +0200

    cambiar rol a enum

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('rol')->default('usuario');
+            // $table->string('rol')->default('usuario');
+            $table->enum('rol', ['usuario', 'administrador'])->default('usuario');
+
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
